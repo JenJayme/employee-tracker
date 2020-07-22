d5c236d299a10f702f5477681f810019c5b7f812
Jen Jayme <jeniferjayme@gmail.com>
2020-07-21 20:38:10 -0700

    Changed initial prompt from if-else to switch-case function to build out menu.

--- app.js
+++ app.js
@@ -34,21 +34,49 @@ var chooseActionPrompt = {
         'Enter Department', 
         'Enter Role', 
         'Enter Employee',
+        'Add Manager',
         'Print All']
 }
 
 // FUNCTION TO RUN FIRST QUESTION
+// function chooseActionFct() {
+//     inquirer.prompt(chooseActionPrompt).then(function (response) {
+//             if (response.action === 'Enter Employee') {
+//                 getEmployeeData();
+//             } else if (response.action === 'Enter Role') {
+//                 getRoleData();
+//             } else if (response.action === 'Enter Department') {
+//                 getDeptData();
+//             } else printData();
+//     });
+// }
+
 function chooseActionFct() {
     inquirer.prompt(chooseActionPrompt).then(function (response) {
-            if (response.action === 'Enter Employee') {
+        switch(response.action){
+            case 'Enter Employee': 
                 getEmployeeData();
-            } else if (response.action === 'Enter Role') {
+                break;
+
+            case 'Enter Role':
                 getRoleData();
-            } else if (response.action === 'Enter Department') {
+                break;
+
+            case 'Enter Department':
                 getDeptData();
-            } else printData();
+                break;
+
+            case 'Add Manager':
+                addManager();
+                break;
+
+            default: 
+                printData();
+        }
     });
 }
+
+
 
 //QUESTION OBJECT TO DETERMINE WHETHER TO ROUTE USER BACK TO ENTER MORE DATA OR FINISH AND PRINT
 var addMorePrompt = {
@@ -176,20 +204,61 @@ function addRole(response) {
             console.log(query.sql);
 }
 
-    function printData() {
-        connection.query('SELECT * FROM employees', function (err, res) {
-            if (err) throw err;
-            console.table(res);
-            connection.end();
-            });
+function printData() {
+    connection.query('SELECT * FROM employees', function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
+        });
+}
+
+function addManager() {
+    connection.query("SELECT * FROM employees", function(err, results) {
+    inquirer.prompt(
+        message: 'For which employee would you like to add a manager?',
+        type: 'rawlist',
+        name: 'employee'
+        choices: 
+        choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+                var fullName = results[i].last_name + ', ' + results[i].first_name;
+                choiceArray.push(results[i].fullName);
+            }
+            return choiceArray;
+        })
+        .then(function(response)
+}
+// =============================================
+
+.then(function(answer) {
+    // get the information of the chosen item
+    var chosenItem;
+    for (var i = 0; i < results.length; i++) {
+      if (results[i].item_name === answer.choice) {
+        chosenItem = results[i];
+      }
     }
 
-    function readColleges() {
-        connection.query("SELECT * FROM colleges", function (err, res) {
-            if (err) throw err;
+// =============================================
 
-            // Log all results of the SELECT statement
-            console.log(res);
-            connection.end();
-            });
+
+
+var managerQuestions = [
+    {
+        message: 'For which employee would you like to add a manager?',
+        type: 'rawlist',
+        name: 'employee'
+        choices: 
+        choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].item_name);
+            }
+            return choiceArray;
+    }, {
+        message: 'Salary:',
+        type: 'input',
+        name: 'salary'
     }
+];
--- scratchpad.js
+++ scratchpad.js
@@ -9,3 +9,40 @@ function getEmployeeData() {
         addEmployee(answers)
     })
 }
+
+// ===================================================
+
+if (response.action === 'Enter Employee') {
+    getEmployeeData();
+} else if (response.action === 'Enter Role') {
+    getRoleData();
+} else if (response.action === 'Enter Department') {
+    getDeptData();
+} else printData();
+
+// ===================================================
+
+function addManager() {
+    connection.query("SELECT * FROM employees", function(err, results) {
+    inquirer.prompt(
+        message: 'For which employee would you like to add a manager?',
+        type: 'rawlist',
+        name: 'employee'
+        choices: 
+        choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+                var fullName = results[i].last_name + ', ' + results[i].first_name;
+                choiceArray.push(results[i].fullName);
+            }
+            return choiceArray;
+        })
+        .then(function(results) {
+            var chosenEmployee;
+            for (var i = 0; i < results.length; i++) {
+                if results[i].fullName === results.choice) {
+                    chosenEmployee = results[i];
+                }
+        }
+    }
+}
